3877ce9db04ef81838a00bd7969af3c57df272bc
Diya <42782061+Diya8@users.noreply.github.com>
2020-11-23 00:15:22 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-consumer_key = "LOv9YkBH471TVpZ1rIXvh2r1C"
-consumer_secret = "qLuGqRtbpabku99KeyGJBSYIv1rkuSX7ijR8ITStg0WuKLeIY8"
-
-access_token = "1080154122664300545-kDrm0PHxLQ0xGSswIsexMCVAi14s2X"
-access_token_secret = "a3fTBREFkPgYZUeG05gpx4OGuvJ1JzWcx69jY751VoWxT"
